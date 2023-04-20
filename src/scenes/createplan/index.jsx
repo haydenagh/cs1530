adffbb03b01b3b1483e09591f463bc0ff6512c74
haydenagh <131166104+haydenagh@users.noreply.github.com>
2023-04-20 16:07:43 -0400

    Add files via upload

--- src/scenes/createplan/index.jsx
+++ src/scenes/createplan/index.jsx
@@ -24,6 +24,7 @@ import { useState } from "react";
     const handleSubmit = (event) => {
       event.preventDefault();
       // Do something with workoutName and selectedExercises
+      alert(`Workout "${workoutName}" created!`);
     };
   
     return (
