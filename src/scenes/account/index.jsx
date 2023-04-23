8906d7d8da81f91c1e0afe53f00ce05b1876a7a2
haydenagh <131166104+haydenagh@users.noreply.github.com>
2023-04-23 17:59:53 -0400

    Add files via upload

--- src/scenes/account/index.jsx
+++ src/scenes/account/index.jsx
@@ -9,7 +9,7 @@ const CreateAccount = () => {
   const [accountCreated, setAccountCreated] = useState(false);
 
   const handleCreateAccount = () => {
-    // Create a new account object with the form data
+    // Form Data
     const newAccount = {
       name: name,
       email: email,
@@ -17,28 +17,29 @@ const CreateAccount = () => {
       password: password
     };
   
-    // Get the existing accounts from local storage
-    const existingAccounts = JSON.parse(localStorage.getItem("accounts")) || [];
+    // Gets existing accounts from local storage
+    const existingAccounts = JSON.parse(localStorage.getItem("accounts")) || []; // This is really cool btw!!
   
-    // Add the new account to the array of existing accounts
+    // Adds new account to the array of existing accounts
     existingAccounts.push(newAccount);
   
-    // Store the updated accounts in local storage
+    // Stores updated accounts in local storage
     localStorage.setItem("accounts", JSON.stringify(existingAccounts));
   
-    // Display a success message to the user
+    // Displays a success message to the user
     alert(`Account created successfully. Welcome, ${name}!`);
   };
   
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
+      {/* Create Account */}
       {accountCreated ? (
         <Typography variant="h4" gutterBottom>
           Welcome, {name}!
         </Typography>
-      ) : (
-        <>
+      ) : ( 
+        <>{/* Completes the Create Account form */}
           <Typography variant="h4" gutterBottom>
             Create Account
           </Typography>
